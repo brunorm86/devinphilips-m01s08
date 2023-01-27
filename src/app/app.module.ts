2f05cdf15f5ab67bed54922422f22a3a44608e57
Bruno Ricardo Machado <bruno_r_machado@estudante.sesisenai.org.br>
2023-01-27 19:39:21 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotificationListComponent } from './components/notification-list/notification-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationListComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule
